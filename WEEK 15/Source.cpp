c2993d1d29d75f31acf2feba7bdc8717266e0504
wittawas19@gmail.com <wittawas19@gmail.com>
2020-11-28 10:41:38 +0700

    Add project files.

---
+++ WEEK 15/Source.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char* str, char* reverse);
+
+int main()
+{
+    char str[1000] = "\0";
+    char reverseStr[1000] = "\0";
+    scanf("%s", &str);
+
+    reverse(str, reverseStr); 
+   
+
+
+}
+
+
+void reverse(char* str,char* reverse)
+{
+
+    for (int i = 0; i < strlen(str); i++)//ระวัง Runtime Error 2 ที่ขนาด Array มีไม่พอ 
+    {
+        for (int j = strlen(str) - i; j >= 0; j--)// หมายถึงการชี้เป้า index จากข้างหลัง 
+        {
+            reverse[j - 1] = str[i]; // ให้ Reverse ตัวอักษร
+            break;
+        }
+    }
+    printf("%s", reverse);
+}
